08a3dbcf115e2668791ebf64adc6abd52271c4ab
Akshatha <>
2020-07-03 03:22:17 +0530

    setting up category and seed data

--- rumbl/lib/rumbl/multimedia.ex
+++ rumbl/lib/rumbl/multimedia.ex
@@ -8,6 +8,9 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Accounts
 
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Category
+
+
 
   @doc """
   Returns the list of video.
@@ -75,6 +78,10 @@ defmodule Rumbl.Multimedia do
     |> Video.changeset(attrs)
     |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing) 
   end
 
   @doc """
