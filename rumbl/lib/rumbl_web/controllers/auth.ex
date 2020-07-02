cdaad983519fc7b5db5ecd27f490501932494b67
Akshatha <>
2020-07-03 00:48:44 +0530

    Loging in

--- rumbl/lib/rumbl_web/controllers/auth.ex
+++ rumbl/lib/rumbl_web/controllers/auth.ex
@@ -10,5 +10,12 @@ defmodule RumblWeb.Auth do
         assign(conn, :current_user, user)
     end
 
+    def login(conn, user) do
+        conn
+        |> assign(:current_user, user)
+        |> put_session(:user_id, user.id)
+        |> configure_session(renew: true)
+    end
+
 end
 
